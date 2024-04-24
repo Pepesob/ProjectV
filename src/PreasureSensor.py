4dcf3868de3f76be89c3a681b424a493c00bc2ff
Pepesob <pepesob2@gmail.com>
2024-04-24 17:41:15 +0200

    Code template for PreasureSensor

---
+++ src/PreasureSensor.py
@@ -0,0 +1,9 @@
+
+class PreasureSensor:
+
+    def __init__(self, gpio_in, gpio_out):
+        pass
+    
+    def update(self):
+        # This function should check input and change output corespondingly; it will be run periodically in the loop
+        pass
